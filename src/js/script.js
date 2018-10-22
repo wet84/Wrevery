717d45d009f631efac68b0207324ed8bd6027c01
Wet <n.user.it@gmail.com>
2018-10-23 01:19:54 +0300

    add slider, fix mobile styles

--- src/js/script.js
+++ src/js/script.js
@@ -23,4 +23,35 @@ $(document).ready(function() {
   $('.menu__links-item').on('click', function() {
     $(this).closest('.menu').removeClass('menu_state_open');
   });
+
+  //slick slider
+  $('.regular').slick({
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    arrows:false,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: '20px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
+
+// $(document).on('ready', function() {
+//   $('.regular').slick({
+//     dots: true,
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+//   });
+// });
+
