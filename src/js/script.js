71e4d31afd2c925ff5219ea2b6e9ce048cc3766b
Wet <n.user.it@gmail.com>
2018-10-23 16:43:48 +0300

    add footer, fix mob styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,3 @@
-
-// (function($){
-//   $(function() {
-//     $('.menu__icon').on('click', function() {
-//       $(this).closest('.menu')
-//         .toggleClass('menu_state_open');
-//     });
-    
-//     $('.menu__links-item').on('click', function() {
-//       // do something
-
-//       $(this).closest('.menu')
-//         .removeClass('menu_state_open');
-//     });
-//   });
-// })(jQuery);
-
 $(document).ready(function() {
   //mobile menu
   $('.menu__icon').on('click', function() {
